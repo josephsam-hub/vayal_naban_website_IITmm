52906c30dc86db46ad523de26b4dca97bf47be79
joseph_sam <josephsam5934@gmail.com>
2026-03-12 02:32:23 +0530

    Fix: Set correct outputDirectory path for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
   }
 })
